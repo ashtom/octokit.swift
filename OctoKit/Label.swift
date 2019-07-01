6551c19b989cfdaea0dfa4a06b680352b1a493a0
Thomas Dohmke <thomas@dohmke.de>
2019-07-01 17:36:02 +0200

    Add custom init to Label.

--- OctoKit/Label.swift
+++ OctoKit/Label.swift
@@ -4,4 +4,8 @@ open class Label: Codable {
     open var url: URL?
     open var name: String?
     open var color: String?
+
+    public init(withName name: String) {
+        self.name = name
+    }
 }
